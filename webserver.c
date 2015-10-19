b42d3ba5caad9532d55d491a118318a937e73831
Vivek <vivek.a.sivakumar@gmail.com>
2015-10-18 17:15:27 -0700

    bug fix on 404

--- webserver.c
+++ webserver.c
@@ -56,7 +56,7 @@ void writeResponse(int sock, char *filename){
 	int filed = open(filename, O_RDONLY);
 	char* statusCode;
 	if (filed < 0){
-		filename = "404.html";
+		strncpy(filename,"404.html",strlen("404.html"));
 		filed = open(filename, O_RDONLY);
 		write(sock, "404 NOT FOUND\n", 14);
 		printf("404 NOT FOUND\n");
